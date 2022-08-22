ee05afb22718b36d730d045c0affc1433de2f974
Steve <steve.maina@student.moringaschool.com>
2022-08-22 20:50:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var employee ={};
+function updateEmployeeWithKeyAndValue(employee= 'Sam', key= 'streetAddress', value= '11 Broadway'){
+    return Object.assign({}, employee, {[key]: value});
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee= 'Sam', key= 'streetAddress', value= '12 Broadway'){
+  employee[key] = value;
+    return employee;
+  }
+function deleteFromEmployeeByKey(employee, key) {
+    var clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone;
+  }
+  
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+
